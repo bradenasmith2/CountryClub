27fa9bfadc41fe21dce41d8f0b0c211bc827d921
bradenasmith2 <bradenasmith@icloud.com>
2023-10-09 12:47:30 -0500

    Added SHOW /api/bookings/:id (SingleBooking()) so that NewBooking() can be manually tested.

--- CountryClubAPI/Controllers/BookingsController.cs
+++ CountryClubAPI/Controllers/BookingsController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CountryClubAPI.DataAccess;
+using CountryClubAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CountryClubAPI.Controllers
 {
     public class BookingsController : Controller
     {
-        public IActionResult Index()
+        private readonly CountryClubContext _context;
+
+        public BookingsController(CountryClubContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [Route("/api/bookings/error")]
+        public string BookingError()
+        {
+            return "Error. Please try again";
+        }
+
+        [HttpPost]
+        [Route("/api/booking")]
+        public IActionResult NewBooking(Booking booking)
+        {
+            if(booking != null) 
+            {
+                _context.Bookings.Add(booking);
+                _context.SaveChanges();
+                return Redirect($"/api/bookings/{booking.Id}");
+            }
+            else
+            {
+                return Redirect("/api/bookings/error");
+            }
+        }
+
+        [Route("/api/bookings/{id:int}")]
+        public IActionResult SingleBooking(int id)
+        {
+            if(id != null)
+            {
+                var booking = _context.Bookings.Find(id);
+
+                return new JsonResult(booking);
+            }
+            else
+            {
+                return Redirect("/api/bookings/error");
+            }
         }
     }
 }
